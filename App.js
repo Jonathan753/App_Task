d0b49f72059b5e7edddca57c0ef439e60bdd5bcb
Jonathan Erik <jonathan753_21@hotmail.com>
2023-10-17 22:51:21 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar, setStatusBarHidden } from 'expo-status-bar';
+import { ImageBackground, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { AntDesign } from '@expo/vector-icons';
+import { useFonts, BalsamiqSans_400Regular, BalsamiqSans_400Regular_Italic, BalsamiqSans_700Bold, BalsamiqSans_700Bold_Italic, } from '@expo-google-fonts/balsamiq-sans';
+//necessario instalação da font antes
 export default function App() {
+  const image = require('./resources/bg.jpg') //caminho da imagem
+
+  let [fontsLoaded] = useFonts({ BalsamiqSans_400Regular, BalsamiqSans_400Regular_Italic, BalsamiqSans_700Bold, BalsamiqSans_700Bold_Italic, });
+  //declarção das fonts
+  if (!fontsLoaded) {
+    return <AppLoading />; //forçar carregamendo da fonts
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={{ flex: 1 }}>
+      <StatusBar hidden style="light" />
+      <ImageBackground source={image} style={styles.image}>
+        <View style={styles.coverView}>
+          <Text style={styles.textHeader}>Lista de Tarefas - Jonathan Érik</Text>
+        </View>
+      </ImageBackground>
+
+      <View style={styles.tarefaSingle}>
+        <View style={{ flex: 1, width: '100%', padding: 10 }}>
+          <Text>Ai dentro</Text>
+        </View>
+        <View style={{ flex: 1, alignItems: 'flex-end', padding: 10 }}>
+          <TouchableOpacity>
+            <AntDesign name="minuscircleo" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  image: {
+    width: '100%',
+    height: 100,
+    resizeMode: 'cover',
   },
+
+  coverView: {
+    width: '100%',
+    height: 100,
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+
+  textHeader: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 20,
+    marginTop: 40,
+    fontFamily: 'BalsamiqSans_400Regular_Italic'
+  },
+
+  tarefaSingle: {
+    marginTop: 30,
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    flexDirection: 'row',
+    paddingBottom: 10
+  }
 });
